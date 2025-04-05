50879b81ed62b0db75231b57a14c4a169a2ed05d
Sodiq Olaniyi <82044466+GaniyuSodiq@users.noreply.github.com>
2025-04-05 09:19:08 +0100

    Add event listener to button container to get each btn clicked

--- script.js
+++ script.js
@@ -41,3 +41,42 @@ function operate(operator, firstNumber, secondNumber) {
 }
 
 // operate("*", 6, 2);
+
+let calcContainer = document.querySelector(".container");
+
+let input = document.querySelector("#input");
+
+let no1 =document.querySelector("#one");
+let no2 =document.querySelector("#two");
+let no3 =document.querySelector("#three");
+let no4 =document.querySelector("#four");
+let no5 =document.querySelector("#five");
+let no6 =document.querySelector("#six");
+let no7 =document.querySelector("#seven");
+let no8 =document.querySelector("#eight");
+let no9 =document.querySelector("#nine");
+let no0 =document.querySelector("#zero");
+
+calcContainer.addEventListener("click", (event) => {
+    let clicked = event.target;
+    switch (clicked.id) {
+        case "one":
+            input.value += 1;
+            break;
+        case "two":
+            input.value += 2;
+            break;
+        case "three":
+            input.value += 3;
+            break;
+        case "four":
+            input.value += 4;
+            break;            
+        default:
+            break;
+    }
+})
+
+// no1.addEventListener("click", (event) => {
+//     input.value += 1;
+// })
